b60646fb70edcf06759d2145d0a3f3a30a86247d
LuuHoang <luugu196@gmail.com>
2016-07-12 17:13:40 +0000

    deploy

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.use(express.static('public'));
 app.use(router);
 
 app.listen(8080, function() {
-    console.log('App is running at https://timestamp-microservice-nguoinaodo.c9users.io'); 
+    console.log('App is running'); 
 });
